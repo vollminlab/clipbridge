1604515b7644903f8a643ccaeecf28d722bef2b2
svollmi1 <scottvollmin@gmail.com>
2026-08-19 04:55:20 +0000

    chore(dotnet): scaffold clipbridge v2 solution and five projects

---
+++ dotnet/ClipBridge.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClipBridge.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ dotnet/ClipBridge.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace ClipBridge.Core;
+
+public class Class1
+{
+
+}
